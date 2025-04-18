cccc360ce06ceb796abed48bf6f699931e4b09ba
Adnan Khan <adnandelhi2004@gmail.com>
2025-04-18 15:41:06 +0300

    Create file_create.sh

---
+++ file_create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##############################
+# Author: Adnan
+# Date: 18-4-2025
+# About: a simple calculator in bash
+##############################
+
+
+for ((i=0; i<=100; i++ ))
+do
+        touch "file_$i.txt"
+done
